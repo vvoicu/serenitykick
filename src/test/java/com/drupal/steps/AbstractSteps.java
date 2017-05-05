a75af74fec99609af7b48f0a570007b8c72b59f3
vvoicu <voicu.vac@evozon.com>
2017-05-05 14:42:45 +0300

    added project navigate to url step

--- src/test/java/com/drupal/steps/AbstractSteps.java
+++ src/test/java/com/drupal/steps/AbstractSteps.java
@@ -1,9 +1,16 @@
 package com.drupal.steps;
 
+import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 
 public class AbstractSteps extends ScenarioSteps{
 
 	private static final long serialVersionUID = 1L;
 
+	
+	
+	@Step
+	public void navigateTo(String url){
+		getDriver().get(url);
+	}
 }
--- src/test/java/com/drupal/tests/account/US001CreateAccountTest.java
+++ src/test/java/com/drupal/tests/account/US001CreateAccountTest.java
@@ -1,7 +1,46 @@
 package com.drupal.tests.account;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.drupal.steps.navigation.NavigationSteps;
 import com.drupal.tests.BaseTest;
+import com.wiki.steps.EndUserSteps;
 
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+
+
+
+@RunWith(SerenityRunner.class)
 public class US001CreateAccountTest extends BaseTest{
+	
 
+    @Steps
+    public EndUserSteps endUserSteps;
+    @Steps
+    public NavigationSteps navigationSteps;
+    
+
+    public String url;
+    public String userName;
+    public String userEmail;
+    public String userPassword;
+    
+    
+    @Before
+    public void dataSetup(){
+    	url = "http://172.22.201.38:8080/";
+    	userName = "";
+    	userEmail = "";
+    	userPassword = "";
+    	
+    }
+
+    
+    @Test
+    public void us001CreateAccountTest(){
+    	navigationSteps.navigateTo(url);
+    }
 }
