9f0ea5a9c41a3df41da565faebf4bbab57b37216
MonaIancu <monalisa.iancu@yahoo.com>
2017-05-08 15:13:16 +0300

    update ShoppingCArtPage

--- src/test/java/com/drupal/pages/cart/ShoppingCartPage.java
+++ src/test/java/com/drupal/pages/cart/ShoppingCartPage.java
@@ -1,15 +1,18 @@
 package com.drupal.pages.cart;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.drupal.pages.AbstractPage;
 
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.annotations.findby.FindBy;
 
 public class ShoppingCartPage extends AbstractPage {
+	WebDriver driver;
 
 	@FindBy(css = ".field.field-name-title-field.field-type-text.field-label-hidden>a")
 	WebElement laptopBagItem;
@@ -17,25 +20,29 @@ public class ShoppingCartPage extends AbstractPage {
 	@FindBy(css = ".field.field-name-title-field.field-type-text.field-label-hidden>a")
 	WebElement commerceToWakeYouUpItem;
 
-	WebElement priceItem;
-	WebElement quantityItem;
-	WebElement totalPricePerProduct;
-	WebElement viewProductItem;
-	WebElement SKUItem;
-	WebElement sizeItem;
-	WebElement colorItem;
+	public WebElement getShoppingCartItem(String productCode) {
 
-	List<Package> shoppingCartList = new ArrayList<Package>();
-
-	public ShoppingCartPage(WebElement priceItem, WebElement quantityItem, WebElement totalPricePerProduct,
-			WebElement viewProductItem, WebElement SKUItem, WebElement sizeItem, WebElement colorItem) {
-		this.priceItem = priceItem;
-		this.quantityItem = quantityItem;
-		this.totalPricePerProduct = totalPricePerProduct;
-		this.viewProductItem = viewProductItem;
-		this.SKUItem = SKUItem;
-		this.sizeItem = sizeItem;
-		this.colorItem = colorItem;
-
+		List<WebElement> cartListElements = getDriver().findElements(By.cssSelector(".views-table.cols-5 tr"));
+		for (WebElement productRowNow : cartListElements) {
+			if (productRowNow.findElement(By.cssSelector("")).getText().toLowerCase()
+					.contentEquals(productCode.toLowerCase())) {
+				return productRowNow;
+			}
+		}
+		return null;
 	}
+
+	public String getPropertyValue(String productCode, String propertyName) {
+		boolean isPropertyFound = false;
+		WebElement shoppingCartItem = getShoppingCartItem(productCode);
+		List<WebElement> propertiesList = shoppingCartItem.findElements(By.cssSelector(""));
+		for (WebElement propertyItem : propertiesList) {
+			propertyItem.getText().toLowerCase().contains(propertyName.toLowerCase() + ":");
+			String propertyParts[] = propertyItem.getText().split(": ");
+			return propertyParts[1];
+		}
+		Assert.assertTrue("The property was not found!", isPropertyFound);
+		return null;
+	}
+
 }
--- src/test/java/tools/Constants.java
+++ src/test/java/tools/Constants.java
@@ -2,8 +2,8 @@ package tools;
 
 public class Constants {
 
-	public static final String USERNAME = "mona123";
-	public static final String PASSWORD = "test123";
+	public static final String USERNAME = "mona1234";
+	public static final String PASSWORD = "mona1234";
 
 	public static final String HOST = "http://172.22.120.86:8080";
 }
