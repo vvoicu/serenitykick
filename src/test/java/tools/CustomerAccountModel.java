c87e4719d8533cfbd89ae2d9a0782542da279947
Victor Toma <victor.toma@evozon.com>
2017-05-12 15:24:26 +0300

    Customer Account Model solved conflicts

---
+++ src/test/java/tools/CustomerAccountModel.java
@@ -0,0 +1,50 @@
+package tools;
+
+public class CustomerAccountModel {
+	
+	private String title;
+	private String firstName;
+	private String lastName;
+	private String day;
+	private String month;
+	private String year;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	
+
+}
